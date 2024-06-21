7f011bf2c83fc9d44b3ec92fddeeeb71a2763fda
Yu-Geun-Chan <geunchan9055@gmail.com>
2024-06-21 09:02:52 +0900

    ílevel0 p120839 문ì풀이

--- src/main/java/com/ki/level0/p120839/Main.java
+++ src/main/java/com/ki/level0/p120839/Main.java
@@ -8,7 +8,19 @@ public class Main {
 class Solution {
     public String solution(String rsp) {
         String answer = "";
-
+        String rsp2 = "";
+        for (int i = 0; i < rsp.length(); i++) {
+            if (rsp.charAt(i) == '2') {
+                rsp2 = "0";
+            }
+            else if (rsp.charAt(i) == '5') {
+                rsp2 = "2";
+            }
+            else if (rsp.charAt(i) == '0') {
+                rsp2 = "5";
+            }
+            answer += rsp2;
+        }
     return answer;
     }
 }
