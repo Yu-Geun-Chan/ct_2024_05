52e986c81fd3bf1ef30623cbc7013ad19804a09d
Yu-Geun-Chan <geunchan9055@gmail.com>
2024-07-11 14:15:47 +0900

    공배수찾기

--- src/main/java/com/ki/level0/p120893/Main.java
+++ src/main/java/com/ki/level0/p120893/Main.java
@@ -7,17 +7,9 @@ public class Main {
 }
 
 class Solution {
-    public String solution(String my_string) {
-        String answer = "";
-        for (int i = 0; i < my_string.length(); i++) {
-            char c = my_string.charAt(i);
-            if (c >= 'a' && c <= 'z') {
-                c = (char) (c - 32);
-            } else {
-                c = (char) (c + 32);
-            }
-            answer = answer + c;
-        }
-        return answer;
+    public int solution(int number, int n, int m) {
+        if (number % n == 0 && number % m == 0) {
+            return 1;
+        } else return 0;
     }
 }
